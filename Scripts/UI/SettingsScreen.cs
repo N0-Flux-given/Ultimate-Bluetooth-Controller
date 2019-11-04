b4deef50327f9eb1c0f60e12f372f731778115be
Ikpreet <ikpreet.s.babra97@gmail.com>
2019-11-04 22:23:36 +0530

    added settings screen buttons

--- Scripts/UI/SettingsScreen.cs
+++ Scripts/UI/SettingsScreen.cs
@@ -7,13 +7,63 @@ public class SettingsScreen : MonoBehaviour
 {
 	private Button btnBack;
 	private CanvasScript canvasReference;
+	public Button btnCross, btnCircle, btnSquare, btnTriangle;
+	public Button btnLeft, btnRight, btnTop, btnBottom;
 
 
 	private void Awake()
 	{
 		LoadReferences();
+		btnCircle.onClick.AddListener(OnClickCircle);
+		btnCross.onClick.AddListener(OnClickCross);
+		btnSquare.onClick.AddListener(OnClickSquare);
+		btnTriangle.onClick.AddListener(OnClickTriangle);
+
+		btnLeft.onClick.AddListener(OnClickLeft);
+		btnRight.onClick.AddListener(OnClickRight);
+		btnLeft.onClick.AddListener(OnClickLeft);
+		btnRight.onClick.AddListener(OnClickRight);
 
 	}
+
+
+	#region Button Click Callbacks
+
+	private void OnClickCircle()
+	{
+
+	}
+	private void OnClickCross()
+	{
+
+	}
+	private void OnClickSquare()
+	{
+
+	}
+	private void OnClickTriangle()
+	{
+
+	}
+	private void OnClickLeft()
+	{
+
+	}
+	private void OnClickRight()
+	{
+
+	}
+	private void OnClickTop()
+	{
+
+	}
+	private void OnClickBotton()
+	{
+
+	}
+
+
+	#endregion
 
 	private void OnEnable()
 	{
